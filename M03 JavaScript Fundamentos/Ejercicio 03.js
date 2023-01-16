06c6503948bc79a7be8e25052f0b183e20669137
johanclavijo <joah1903@gmail.com>
2023-01-16 15:58:00 -0500

    16 de enero Ejercicio 1,2,3

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -2,40 +2,60 @@
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
-   // Retorna True si "x" e "y" son iguales.
-   // De lo contrario, retorna False.
-   // Tu código:
+   if (x == y) {
+      return true;
+   } else { 
+      return false;
+   }
 }
+//console.log(sonIguales(5,5));
 
 function tienenMismaLongitud(str1, str2) {
-   // Retorna True si los dos strings tienen la misma longitud.
-   // De lo contrario, retorna False.
-   // Tu código:
+   if (str1.length == str2.length) {
+      return true;
+   } else {
+      return false;
+   }
 }
+//console.log(tienenMismaLongitud("johan","johan"));
+
 
 function menosQueNoventa(num) {
-   // Retorna True si el argumento "num" es menor que noventa.
-   // De lo contrario, retorna False.
-   // Tu código:
+   if (num < 90) {
+      return true;
+   } else {
+      return false;
+   }
 }
+//console.log(menosQueNoventa(56));
 
 function mayorQueCincuenta(num) {
-   // Retorna True si el argumento "num" es mayor que cincuenta.
-   // De lo contrario, retorna False.
-   // Tu código:
+   if (num > 50) {
+      return true;
+   } else {
+      return false;
+   }
 }
+//console.log(mayorQueCincuenta(40));
 
 function esPar(num) {
-   // Retorna True si "num" es par.
-   // De lo contrario, retorna False.
-   // Tu código:
+   if (num % 2 == 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
+//console.log(esPar(40));
 
 function esImpar(num) {
-   // Retorna True si "num" es impar.
-   // De lo contrario, retorna False.
-   // Tu código:
+   if (num % 2 != 0) {
+      return true;
+   } else {
+      return false;
+   }
+   
 }
+//console.log(esImpar(9));
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
